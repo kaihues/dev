d2329b4aba86d8bab1e82e2e511ccacc9f48f7c7
Kai Hughes <70239072+kaihues@users.noreply.github.com>
2023-10-16 22:33:58 -0600

    added images + animation

--- assignments/assignment5/src/index.js
+++ assignments/assignment5/src/index.js
@@ -1,7 +1,40 @@
 import './stylesheet.css';
-import click from '../images/click-letter.png';
+import duck1 from '../images/duck1.png';
+import duck2 from '../images/duck2.png';
+import duck3 from '../images/duck3.png';
+import duck4 from '../images/duck4.png';
+import duck5 from '../images/duck5.png';
 
-document.getElementById('click-image').src=click;
+duck = document.getElementById('duck-image');
+duck.src="duck1";
 
-console.log('hello world');
-console.log('yeah');  
+let mouseState = 1;
+
+duck.addEventListener('mouseover', () => {
+    mouseState = 2;
+    mouseState = 3;
+});
+
+duck.addEventListener('mouseout', () => {
+    mouseState = 2;
+    mouseState = 1;
+});
+
+while (mouseState = 1) {
+    duck.src = duck1;
+    wait(500);
+    duck.src = duck2;
+    wait(500);
+}
+
+while (mouseState = 2) {
+    duck.src = duck3;
+    wait(500);
+}
+
+while (mouseState = 3) {
+    duck.src = duck4;
+    wait(500);
+    duck.src = duck5;
+    wait(500);
+}
