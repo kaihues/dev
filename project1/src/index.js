1f30986277f5c396f5d88e694e48750efbcd32f6
Kai Hughes <70239072+kaihues@users.noreply.github.com>
2023-10-24 00:47:31 -0600

    button work

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,3 +1,7 @@
 import './style.css';
 
 console.log('hello world');
+
+let sButton = document.getElementById(stemEdButton);
+let tButton = document.getElementById(techButton);
+let dButton = document.getElementById(designButton);
